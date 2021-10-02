f4daffc66b04193c671bb606731f415ddcfd4677
AstritMuciqi1 <astritmuciqi@gmail.com>
2021-10-02 14:02:12 +0200

    Krijimi i nje faqe per shikimin e produkteve ne detale nga produktet qe jane visible ne Home Page.

--- client-app/src/feautures/Details/DetailsShow/ProductDetails.tsx
+++ client-app/src/feautures/Details/DetailsShow/ProductDetails.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { Fragment, useContext, useEffect } from "react";
-import {  RouteComponentProps } from "react-router-dom";
-import { Card, Button, Image, Icon } from "semantic-ui-react";
+import { RouteComponentProps } from "react-router-dom";
+import {  Button, Icon, Item } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ProductStore from "../../../app/stores/productStore";
 import Footer from "../../footer/Footer";
@@ -19,28 +19,53 @@ const ProductDetails: React.FC<RouteComponentProps<DetailParams>> = ({
 
   useEffect(() => {
     loadProduct(match.params.id);
-  }, [loadProduct, match.params.id,history]);
+  }, [loadProduct, match.params.id, history]);
 
-  if (loadingInitial || !product )
+  if (loadingInitial || !product)
     return <LoadingComponent content="Loading Product..." />;
   return (
     <Fragment>
       <div style={{ height: "100%" }}>
         <Navbar />
-        <Card fluid style={{ marginTop: "16em" }}>
-          <Image
-            style={{ width: "275px", height: "220px" }}
-            src={`/assets/${product.sector}.jpg`}
-          />
-          <Card.Content>
-            <Card.Header>{product.productName}</Card.Header>
-            <Card.Meta>
-              <span>{product.modelYear}</span>
-            </Card.Meta>
-            <Card.Description>{product.description}</Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <Button positive>
+        <Item.Group fluid style={{ marginTop: "16em",backgroundColor:"white" }}>
+        
+          <Item>
+            <Item.Image style={{borderRight:"5px solid black"}} size="big" src={`/assets/${product.sector}.jpg`} />
+
+            <Item.Content>
+              <Item.Header as="a" style={{marginTop:"50px",fontSize:"30px"}}>{product.brand} {product.productName}</Item.Header>
+              <Item.Meta style = {{marginTop:"20px"}}>Çmimi i produktit:
+              <p style={{color:"green",fontSize:"20px",marginTop:"10px"}}> {product.valueOfProduct}$</p>
+              </Item.Meta>
+              <Item.Meta>Viti i prodhimit:
+              <p style={{fontSize:"20px",color:"black",marginTop:"10px"}}>{product.modelYear}</p>
+              </Item.Meta>
+              <Item.Meta >
+                Brendi:
+                <p style={{fontSize:"20px",marginTop:"10px",color:"black"}}>
+                  {product.brand}
+                </p>
+              </Item.Meta>
+              <Item.Meta >
+                Sektori:
+                <p style={{fontSize:"20px",marginTop:"10px",color:"black"}}>
+                  {product.sector}
+                </p>
+              </Item.Meta>
+              <Item.Meta >
+                Pershkrimi i produktit:
+                <p style={{fontSize:"20px",marginTop:"10px",color:"black"}}>
+                  {product.description}
+                </p>
+              </Item.Meta>
+              <Item.Meta >
+                Disponushmeria:
+                <p style={{fontSize:"20px",marginTop:"10px",color:"black"}}>
+                  {product.quantity} artikuj
+                </p>
+              </Item.Meta>
+              <Item.Extra extra style={{marginTop:"40px"}}>
+             <Button positive>
               Add to Cart{" "}
               <Icon
                 style={{ marginLeft: "7px", marginBottom: "3px" }}
@@ -49,8 +74,11 @@ const ProductDetails: React.FC<RouteComponentProps<DetailParams>> = ({
                 size="large"
               />
             </Button>
-          </Card.Content>
-        </Card>
+          </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+
         <Footer />
       </div>
     </Fragment>
